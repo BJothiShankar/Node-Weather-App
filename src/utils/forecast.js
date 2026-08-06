1a572e5ef7e31dca358a00b505db03d9f9b3e5dd
BJothiShankar <shankar.xordicanz@gmail.com>
2026-08-06 10:40:41 +0530

    Remove temporary test route

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,11 +14,11 @@ const forecast = (latitude, longitude, callback) => {
         body.current.weather_descriptions[0] +
           ". It is currently " +
           body.current.temperature +
-          " degrees out.It feels like " +
+          " degrees out. It feels like " +
           body.current.feelslike +
           " degrees out. The humidity is " +
           body.current.humidity +
-          "%.",
+          "% .",
       );
     }
   });
